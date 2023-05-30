2b63bad3e67d07e78a8042a9b4578c05ab4f72d4
CamJam <cjetton18@gmail.com>
2023-05-30 09:39:18 -0400

    more URL testing

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'production' => true,
-    'baseUrl' => 'https://github.com/CamJam-01/borg-cube/tree/live-site',
+    'baseUrl' => 'https://github.com/CamJam-01/borg-cube/tree/gh-pages',
     'title' => 'Borg Cube',
     'description' => 'Resistance is futile.',
     'collections' => [],
